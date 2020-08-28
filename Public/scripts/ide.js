4fbbfb1b231d437c64d967954c7dcea4bbbd37a5
Tomás Marques <48774061+TomascpMarques@users.noreply.github.com>
2020-08-28 08:39:25 +0100

    editor Monaco implementado
    Adicionei as keywords, typeKeywords, operators e o tokenizer.
    
    Mudei algumas Cores do tema, criei o tema PseudoPT para o editor.
    E mudei o default text do editor

--- Public/scripts/ide.js
+++ Public/scripts/ide.js
@@ -14,35 +14,130 @@ require(['vs/editor/editor.main'], function() {
 
 
     monaco.languages.setMonarchTokensProvider('PseudoPT', {
+        keywords: [
+            'Funcao', 'por', 'falsso', 'enquanto', 'verdade',
+            'receber', 'enviar', 'devolver', 'se', 'senao',
+        ],
+
+        typeKeywords: [
+            'booleano', 'inteiro', 'real', 'vetor', 'string', 'caracter'
+        ],
+
+        operators: [
+            '=', '>', '<', '!', '', '<=', '>=', 'diferente',
+            'e', 'ou', '+', '-', '*', '/', ':'
+        ],
+
+        symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+        escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
         tokenizer: {
-            type
-
-            operators: [
-                '=', '>', '<', '!', '~', '?', ':', '==', '<=', '>=', '!=',
-                '&&', '||', '++', '--', '+', '-', '*', '/', '&', '|', '^', '%',
-                '<<', '>>', '>>>', '+=', '-=', '*=', '/=', '&=', '|=', '^=',
-                '%=', '<<=', '>>=', '>>>='
-            ],
-
             root: [
+                // identifiers e keywords
+                [/[a-z_$][\w$]*/, {
+                    cases: {
+                        '@typeKeywords': 'keyword',
+                        '@keywords': 'keyword',
+                        '@default': 'identifier'
+                    }
+                }],
+                [/[A-Z][\w\$]*/, 'type.identifier'], // mostra bonito os nomes das classes 
 
+                // espaço
+                { include: '@whitespace' },
+
+                // delimitadores e operadores
+                [/[{}()\[\]]/, '@brackets'],
+                [/[<>](?!@symbols)/, '@brackets'],
+                [/@symbols/, {
+                    cases: {
+                        '@operators': 'operator',
+                        '@default': ''
+                    }
+                }],
+
+                // @ annotations.
+                // As an example, we emit a debugging log message on these tokens.
+                // Note: message are supressed during the first load -- change some lines to see them.
+                [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+
+                // numbers
+                [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+                [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+                [/\d+/, 'number'],
+
+                // delimiter: after number because of .\d floats
+                [/[;,.]/, 'delimiter'],
+
+                // strings
+                [/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+                [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+                // characters
+                [/'[^\\']'/, 'string'],
+                [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+                [/'/, 'string.invalid']
             ],
-        }
+
+            comment: [
+                [/[^\/*]+/, 'comment'], //comentário
+                [/\/\*/, 'comment', '@push'], // nested comment
+                ["\\*/", 'comment', '@pop'],
+                [/[\/*]/, 'comment']
+            ],
+
+            string: [
+                [/[^\\"]+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+            ],
+
+            whitespace: [
+                [/[ \t\r\n]+/, 'white'],
+                [/\/\*/, 'comment', '@comment'],
+                [/\/\/.*$/, 'comment'],
+            ],
+        },
     });
+
+    monaco.editor.defineTheme('PseudoTema', {
+        base: 'vs-dark',
+        inherit: true,
+        rules: [
+            { token: 'string', foreground: '056CF2' },
+            { token: 'comment', foreground: '99ff33' },
+            { token: 'number', foreground: '66ffff' },
+            { token: 'delimiter', foreground: 'ffff00' },
+            { token: 'identifier', foreground: 'e6ffe6' },
+            { token: 'operator', foreground: 'ffa64d' },
+        ]
+    });
+
+    /**
+     *  FALTA O AUTOCOMPLETE
+     */
 
     window.editor = monaco.editor.create(document.getElementById('container'), {
         //texto inicial do editor
         value: [
-            'function x() {',
-            '\tconsole.log("Hello world!");',
+            '// Função x com saída',
+            'Funcao x() {',
+            '\tenviar("Olá Mundo");',
+            '\tdevolver 0;',
+            '}',
+            '\n// Estrutura de decisão \'IF\'',
+            'se 5 + 4 igual 9 {',
+            '\tenviar(5+4);',
             '}'
         ].join('\n'),
         //a linguagem do Pseudo-PT
         language: 'PseudoPT',
         //Aspeto do editor
-        theme: "vs-dark",
+        theme: "PseudoTema",
         //Aparência do Texto
-        fontSize: '18px',
+        fontSize: '20px',
         fontWeight: 'bold',
     });
 });
