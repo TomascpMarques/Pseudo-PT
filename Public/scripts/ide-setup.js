d5a43eb31f5b7609c5f839f15338c5e9977b701c
Tomás Marques <48774061+TomascpMarques@users.noreply.github.com>
2020-08-28 17:31:53 +0100

    HTML/Pseudo.js
    Adiconados butões para a mobilidade do utilizador na página
    
    Adicionado a textarea de output para quando o código for executado
    
    E finalizado o parser de conteudo do editor.

--- Public/scripts/ide-setup.js
+++ Public/scripts/ide-setup.js
@@ -231,14 +231,13 @@ require(['vs/editor/editor.main'], function() {
         //texto inicial do editor
         value: [
             '// Função x com saída //',
-            'Func x() {',
-            '\tenviar("Olá Mundo")',
-            '\tdevolver 0',
-            '}',
+            'Func x()',
+            '\tenviar("Olá Mundo");',
+            '\tdevolver (0);',
+            '',
             '\n// Estrutura de decisão \'IF\'//',
-            'se (5 + 4 igual 9) {',
-            '\tenviar(5+4)',
-            '}'
+            'se (5 + 4 igual 9)',
+            '\tenviar(5+4);',
         ].join('\n'),
         //a implementação do Pseudo-PT
         language: 'PseudoPT',
